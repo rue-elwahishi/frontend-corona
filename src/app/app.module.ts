e24e23792f8b3a6e5689b25146a70a9926ff7638
unknown <assma.bm@gmail.com>
2020-03-23 15:32:41 +0200

    fixed sidebar and router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent, SidebarComponent],
   imports: [
