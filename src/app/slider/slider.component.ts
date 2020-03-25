5b35b52197cd9d77cc13b9a07ed26220dfcbc373
rue-elwahishi <rubawahishi@gmail.com>
2020-03-25 19:58:54 +0200

    working on slider

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { ImageCroppedEvent } from "ngx-image-cropper";
 
 @Component({
   selector: "app-slider",
@@ -7,20 +6,18 @@ import { ImageCroppedEvent } from "ngx-image-cropper";
   styleUrls: ["./slider.component.css"]
 })
 export class SliderComponent implements OnInit {
-  title = "angular-image-uploader";
-  imageChangedEvent: any = "";
-  croppedImage: any = "";
   default: any =
     "https://filestore.community.support.microsoft.com/api/images/72e3f188-79a1-465f-90ca-27262d769841";
 
   constructor() {}
 
   ngOnInit() {}
+  upload(e: any, img) {
+    var input = e.target;
+    console.log(e);
 
-  fileChangeEvent(event: any): void {
-    this.imageChangedEvent = event;
-  }
-  imageCropped(event: ImageCroppedEvent) {
-    this.croppedImage = event.base64;
+    if (input.files && input.files[0]) {
+      img.src = URL.createObjectURL(input.files[0]);
+    }
   }
 }
